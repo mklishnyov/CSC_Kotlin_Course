bb53abcaac248edda64ba535f6ae5d12d060ce77
Matvey <klishnev2018@gmail.com>
2025-08-22 09:03:49 +0500

    22.08.25

--- AlmostDone/task/src/main/kotlin/jetbrains/kotlin/course/almost/done/Main.kt
+++ AlmostDone/task/src/main/kotlin/jetbrains/kotlin/course/almost/done/Main.kt
@@ -1,5 +1,30 @@
 package jetbrains.kotlin.course.almost.done
 
+fun trimPicture(picture: String): String = picture.trimIndent()
+
+fun applyBordersFilter(picture: String): String {
+    val ap = StringBuilder()
+    val at = picture.lines()
+    ap.append("${borderSymbol.toString().repeat(getPictureWidth(picture) + 4)}$newLineSymbol")
+    for (line in at) {
+        val padded = line.padEnd(getPictureWidth(picture), separator)
+        ap.append("$borderSymbol$separator$padded$separator$borderSymbol$newLineSymbol")
+    }
+
+    ap.append(borderSymbol.toString().repeat(getPictureWidth(picture) + 4))
+    return ap.toString()
+}
+
+fun applySquaredFilter(picture: String): String = TODO()
+
+fun applyFilter(picture: String, filter: String): String {
+    return when (filter) {
+        "borders" -> applyBordersFilter(trimPicture(picture))
+        "squared" -> applySquaredFilter(trimPicture(picture))
+        else -> error("Wrong filter name")
+    }
+}
+
 fun main() {
     // Uncomment this code on the last step of the game
 
